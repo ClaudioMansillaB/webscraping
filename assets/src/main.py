bc12c51d9552efee411261bb911e42b3e8f3032b
Claudio Mansilla <c.mansillabrito@gmail.com>
2024-04-03 19:55:41 -0300

    added commands to get data and initiated arrays to save the data

--- assets/src/main.py
+++ assets/src/main.py
@@ -1,8 +1,29 @@
-def main():
+import requests 
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def main(link: str):
     """
     This file is dedicated to web scraping the data from the website.
     """
+    
+    reply = requests.get(link)
+    content = reply.text
+    soup = BeautifulSoup(content, "html.parser")
 
+    lesson_list = soup.find_all("div", class_ = "lesson-description")
+
+    titles = []
+    authors = []
+    summary = []
+    links = []
+    dates = []
+    topics = []
+    activites = []
+    difficulties = []
 
 if __name__ == "__main__":
-    main()
+    link = "http://programminghistorian.org/es/lecciones/"
+    main(
+        link = link
+        )
